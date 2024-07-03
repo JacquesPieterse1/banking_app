4252e7a4760789b35229f8d6064c67ff22e051eb
Jacques Pieterse <jacquespieterse2000@gmail.com>
2024-07-03 12:06:07 +0200

    Implement complete layout with sidebars

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
 import TotalBalanceBox from '@/components/ui/TotalBalanceBox';
 import HeaderBox from '@/components/ui/HeaderBox'
 import React from 'react'
+import RightSidebar from '@/components/ui/RightSidebar';
 
 const Home = () => {
 
-  const loggedIn = { firstName: 'Jacques'};
+  const loggedIn = { firstName: 'Jacques', lastName: "Pieterse", email: "jpieterse@gmail.com"};
   return (
     <section className="home">
       <div className='home-content'>
@@ -22,7 +23,13 @@ const Home = () => {
             totalCurrentBalance={1250.35}
           />
         </header>
+
+        Recent Transactions
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance: 123.50}, {currentBalance: 500.00}]}/>
     </section>
   )
 }
